1e5915212037c3f0c828d396463250670e03fae7
Truong Hoang Dung <revskill09@gmail.com>
2013-12-09 14:44:02 +0700

    Update API

--- app/controllers/api/v1/sinhvien_controller.rb
+++ app/controllers/api/v1/sinhvien_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SinhvienController < ActionController::Base
 	def thamdo
-    @sinh_viens = Sinhvien.by_masinhvien(params[:id].strip).by_voted.pluck(:tenmon)
+    	@sinh_viens = Sinhvien.by_masinhvien(params[:id].strip).by_voted.select([:id,:tenmon, :survey_id, :mamon])
 
     respond_to do |format|      
       format.json { render json: @sinh_viens }
