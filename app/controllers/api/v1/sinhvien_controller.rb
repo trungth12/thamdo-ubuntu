a7458fdff40d49baa23a1fc94d78a96b2e9a8354
Truong Hoang Dung <revskill09@gmail.com>
2013-12-11 14:42:13 +0700

    Update api

--- app/controllers/api/v1/sinhvien_controller.rb
+++ app/controllers/api/v1/sinhvien_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SinhvienController < ActionController::Base
 	def thamdo
-    	@sinh_viens = Sinhvien.by_masinhvien(params[:id].strip).by_voted.select([:id,:tenmon, :survey_id, :mamon])
+		surveyid = Survey.by_activated
+    	@sinh_viens = Sinhvien.where(survey_id: surveyid).by_masinhvien(params[:id].strip).by_voted.select([:id,:tenmon, :survey_id, :mamon])
 
     respond_to do |format|      
       format.json { render json: @sinh_viens }
